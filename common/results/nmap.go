41f1038d45e4e2123153849e3397d41a4970a164
grez-lucas <lucasgrez3010@gmail.com>
2024-12-31 17:15:25 -0300

    feat(nmap) Add basic vulnerability struct

--- common/results/nmap.go
+++ common/results/nmap.go
@@ -1,16 +1,17 @@
 package results
 
 type NmapResult struct {
-	HostName     string         `json:"host_name"`
-	HostAddress  string         `json:"host_address"`
-	ScannedPorts []NmapPortData `json:"scanned_ports"`
-	OSFamily     string         `json:"os_family"`
+	HostName     string     `json:"host_name"`
+	HostAddress  string     `json:"host_address"`
+	ScannedPorts []PortData `json:"scanned_ports"`
+	OSFamily     string     `json:"os_family"`
 }
 
-type NmapPortData struct {
+type PortData struct {
 	ID       uint16  `xml:"portid,attr" json:"id"`
 	Protocol string  `xml:"protocol,attr" json:"protocol"`
 	Service  Service `xml:"service" json:"service"`
+	Product  string  `xml:"product" json:"product"`
 	State    string  `xml:"state" json:"state"`
 }
 
@@ -18,4 +19,11 @@ type Service struct {
 	Name       string `json:"name"`
 	Version    string `json:"version"`
 	Confidence int    `json:"confidence"`
+}
+
+type Vulnerability struct {
+	ID         string   `json:"id"`
+	CVSS       float64  `json:"cvss"`
+	References []string `json:"reference"`
+	HasExploit bool     `json:"has_exploit"`
 }
