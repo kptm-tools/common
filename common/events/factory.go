261a5c717405132b57bdee8d1fe7dfc67c8cb95b
grez-lucas <lucasgrez3010@gmail.com>
2025-01-21 16:50:49 -0300

    fix: Remove NewToolResultEvent error handling

--- common/events/factory.go
+++ common/events/factory.go
@@ -4,35 +4,22 @@ import (
 	"encoding/json"
 	"time"
 
-	"github.com/kptm-tools/common/common/enums"
 	"github.com/kptm-tools/common/common/results"
 )
 
 type ToolEventFactory struct{}
 
 func (f *ToolEventFactory) BuildEvent(scanID string, toolResult results.ToolResult, err error) ([]byte, error) {
-	evt := NewToolResultEvent(scanID, toolResult, err)
+	evt := NewToolResultEvent(scanID, toolResult)
 	return json.Marshal(evt)
 }
 
-func NewToolResultEvent(scanID string, toolResult results.ToolResult, err error) ToolResultEvent {
+func NewToolResultEvent(scanID string, toolResult results.ToolResult) ToolResultEvent {
 	return ToolResultEvent{
 		BaseEvent: BaseEvent{
 			ScanID:    scanID,
 			Timestamp: time.Now().Unix(),
-			Error:     getEventError(err),
 		},
 		ToolResult: toolResult,
 	}
-}
-
-// getEventError is a helper function to map the error result to a Service EventError
-func getEventError(err error) *EventError {
-	if err != nil {
-		return &EventError{
-			Code:    enums.ToolError,
-			Message: err.Error(),
-		}
-	}
-	return nil
 }
