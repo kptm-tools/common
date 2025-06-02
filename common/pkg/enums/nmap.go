76596f0686125da27ff59689c68792038b986645
arnold <luizarnoldch@gmail.com>
2025-06-02 13:25:21 -0500

    add the enum to the parse of metrics and init the array on 0

--- common/pkg/enums/nmap.go
+++ common/pkg/enums/nmap.go
@@ -1,5 +1,7 @@
 package enums
 
+import "fmt"
+
 // CVSSVersion representa la versión de CVSS que aceptamos en nuestros métricos.
 type CVSSVersion string
 
@@ -8,3 +10,25 @@ const (
 	CVSSv30 CVSSVersion = "3.0"
 	CVSSv20 CVSSVersion = "2.0"
 )
+
+func IsValidVersion(v CVSSVersion) bool {
+	switch v {
+	case CVSSv31, CVSSv30, CVSSv20:
+		return true
+	default:
+		return false
+	}
+}
+
+// ParseCVSSVersion convierte un string a CVSSVersion, o devuelve error si no existe
+func ParseCVSSVersion(s string) (CVSSVersion, error) {
+	switch CVSSVersion(s) {
+	case CVSSv31:
+		return CVSSv31, nil
+	case CVSSv30:
+		return CVSSv30, nil
+	case CVSSv20:
+		return CVSSv20, nil
+	}
+	return "", fmt.Errorf("versión CVSS inválida: %q", s)
+}
